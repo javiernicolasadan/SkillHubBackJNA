c2ba5b0b0c9df0c176844005da86ab04eeb6075e
Elisa Exposito <eliexme@gmail.com>
2023-05-19 14:40:39 +0100

    skill routes

--- routes/skill.routes.js
+++ routes/skill.routes.js
@@ -1,22 +1,23 @@
 const Skill = require("../models/Skill.model");
 const router = require("express").Router();
 
+/* router.get("/", (req, res, next) => {
+  res.json("Skill good in here");
+}); */
+
 router.get("/", (req, res, next) => {
   res.json("Skill good in here");
 });
 
-// POST  to add one Skill 
-  router.post('/create', async (req, res) => {
-    const payload = req.body
-    try {
-      const newSkill = await Skill.create(payload)
-      res.status(201).json(newSkill)
-    } catch (error) {
-      console.log(error)
-    }
-  })
-
-
-
+// POST  to add one Skill
+router.post("/create", async (req, res) => {
+  const payload = req.body;
+  try {
+    const newSkill = await Skill.create(payload);
+    res.status(201).json(newSkill);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;

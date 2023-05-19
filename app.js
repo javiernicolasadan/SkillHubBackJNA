01282c877ec7bd04bb98508556124dd549cbb7f4
Elisa Exposito <eliexme@gmail.com>
2023-05-19 10:52:03 +0100

    skill route create

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use("/", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
+
+const skillRoutes = require("./routes/skill.routes");
+app.use("/skill", skillRoutes);
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
---
+++ routes/skill.routes.js
@@ -0,0 +1,22 @@
+const Skill = require("../models/Skill.model");
+const router = require("express").Router();
+
+router.get("/", (req, res, next) => {
+  res.json("Skill good in here");
+});
+
+// POST  to add one Skill 
+  router.post('/create', async (req, res) => {
+    const payload = req.body
+    try {
+      const newSkill = await Skill.create(payload)
+      res.status(201).json(newSkill)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+
+
+
+module.exports = router;

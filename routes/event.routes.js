83c01292b6d02fa9ee907b6cce74660399d327f7
Javier <javiernicolasadan@gmail.com>
2023-05-21 15:57:48 +0200

    event details, update, delete

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,9 +1,6 @@
 const Event = require("../models/Event.model");
 const router = require("express").Router();
 
-/* router.get("/", (req, res, next) => {
-  res.json("Event good in here");
-}); */
 
 router.get("/", (req, res, next) => {
   res.json("Event good in here");
@@ -11,8 +8,9 @@ router.get("/", (req, res, next) => {
 
 // POST  to add one Event
 router.post("/create", async (req, res) => {
-  const payload = req.body;
+  
   try {
+    const payload = req.body;
     const newEvent = await Event.create(payload);
     res.status(201).json(newEvent);
   } catch (error) {
@@ -20,4 +18,57 @@ router.post("/create", async (req, res) => {
   }
 });
 
+//GET event details
+router.get("/eventdets/:eventId", async (req, res) => {
+    
+  try {
+    const eventId = req.params.eventId
+    const eventDetails = await Event.findById(eventId )
+    const { _id, title, description, date, locationType } = eventDetails;
+    const formattedDate = date.toISOString().slice(0, 10)
+    const formattedEventDetails = {
+      _id,
+      title,
+      description,
+      date: formattedDate,
+      locationType,
+    }
+    res.status(200).json(formattedEventDetails)
+      
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+// POST edit event
+router.put ("/updateevent/:eventId", async (req, res) => {
+
+  try {
+    const eventId = req.params.eventId
+    const updateDataEvent = req.body
+    const updatedEvent = await Event.findByIdAndUpdate(
+      eventId,
+      {
+       title: updateDataEvent.title,
+       description: updateDataEvent.description,
+       date: updateDataEvent.date,
+       locationType: updateDataEvent.locationType,
+
+      }, {new:true})
+      res.status(200).json(updatedEvent)
+  } catch (error) {
+    console.log(error)
+  }
+
+})
+
+router.delete ("/deleteevent/:eventId", async (req, res) => {
+  try{
+    const eventId = req.params.eventId
+
+    await Event.findByIdAndDelete(eventId)
+    res.status(200).json({message: "Event deleted"})
+  } catch (error) {
+    console.log(error)
+}})
 module.exports = router;
